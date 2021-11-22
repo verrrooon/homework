26f62ecf1bb38b7135d4c0d7c1d57b9dacb715d2
verrrooon <verrrooon@yandex.ru>
2021-11-23 00:08:05 +0300

    Добавлено задание 2

---
+++ homework3/Ex-2.py
@@ -0,0 +1,32 @@
+print("Для остановки введите стоп")
+stop = 0
+check = ["влево", "вправо", "вверх", "вниз"]
+coordinates = [0,0]
+while stop == 0:
+    direction = input("Введите направление: влево, вправо, вверх, вниз \n")
+    if direction == "стоп":
+        stop = 1
+        break
+    if direction not in check:
+        print("Введите корректное направление!")
+    else:
+
+        quantity = float(input("Введите число шагов \n"))
+
+        if  quantity < 0 :
+            print("Количество шагов не может быть отрицательным!")
+        else:
+
+            if direction == "влево":
+                coordinates[0] = coordinates[0] - quantity
+
+            elif direction == "вправо":
+                coordinates[0] = coordinates[0] + quantity
+
+            elif direction == "вверх":
+                coordinates[1] = coordinates[1] + quantity
+
+            elif direction == "вниз":
+                coordinates[1] = coordinates[1] - quantity
+
+print("Конечные координаты =", coordinates)
