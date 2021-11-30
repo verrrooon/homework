7c4540c972b40f04207bf6e6b37cdc9f5b1a31b9
verrrooon <verrrooon@yandex.ru>
2021-11-30 23:06:15 +0300

    Добавлено задание 2

---
+++ homework4/Ex-2.py
@@ -0,0 +1,4 @@
+rgb = {'White': (255, 255, 255),'Blue': (0, 0, 255),'Green': (0, 255, 0),'Yellow': (255, 255, 0), 'Red': (255, 0, 0),'Black': (0, 0, 0) }
+
+for key, val in rgb.items():
+    print(f'{key}, RGB: {val}')
