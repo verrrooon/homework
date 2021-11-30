31949576f1e9f132e5cfcdf78b890efd7811a9db
verrrooon <verrrooon@yandex.ru>
2021-12-01 00:29:56 +0300

    Добавлено задание 3

---
+++ homework4/Ex-3.py
@@ -0,0 +1,10 @@
+from random import randint
+
+a = {randint(0, 15) for _ in range(15)}
+b = {randint(0, 15) for _ in range(15)}
+print('Первое множество', a)
+print('Второе множество',b)
+print('входят одновременно в оба ', a & b)
+print('входят только в первое, но не входят во второе; ',a.difference(b))
+print('входят только во второе, но не входят в первое; ',b.difference(a))
+print('входят в первое или во второе, но не в оба из них одновременно; ', a.symmetric_difference(b))
