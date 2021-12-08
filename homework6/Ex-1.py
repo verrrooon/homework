4f86d479105395ff365300cebd7e1b8a537a50c7
verrrooon <verrrooon@yandex.ru>
2021-12-08 12:46:49 +0300

    Добавлено задание 1

---
+++ homework6/Ex-1.py
@@ -0,0 +1,12 @@
+
+a = input('Введите cписок строк: ').split()
+def straight(a):
+    for i in range(len(a)):
+        a[i] = a[i].encode('utf-8')
+    return a
+def back(a):
+    for i in range(len(a)):
+        a[i] = a[i].decode('utf-8')
+    return a
+print(f'Прямое преобразование: {straight(a)}')
+print(f'Обратное преобразование:{back(a)}')
